65eada5f0201e08f09d282b1a66b3b41bb9cd673
GaelScript <gaelmota@dusty.tamiu.edu>
2025-11-17 18:36:02 -0600

    Three scripts to run the program normally, run the program with valgrind, and another with gdb. Important to note: you should compile with "CXXFLAGS="-g"" for debug info.

---
+++ run.sh
@@ -0,0 +1,4 @@
+#bin/champsim_bypass --warmup-instructions 2000000 --simulation-instructions 5000000 /home/navi/Dev/ChampSim-UIP_R/649.fotonik3d_s-1B.champsimtrace.xz
+bin/champsim_bypass --warmup-instructions 200000 \
+--simulation-instructions 500000 \
+/home/navi/Dev/ChampSim-UIP_R/649.fotonik3d_s-1B.champsimtrace.xz
---
+++ run_valgrind.sh
@@ -0,0 +1,5 @@
+valgrind --tool=memcheck --leak-check=full --track-origins=yes \
+  ./bin/champsim_bypass --warmup-instructions 200000 \
+  --simulation-instructions 500000 \
+  /home/navi/Dev/ChampSim-UIP_R/649.fotonik3d_s-1B.champsimtrace.xz \
+  2> valgrind-out.txt
